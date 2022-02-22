dfa522a777c85bf15da8fbd756196c494099fd6b
Chaitali Sakharkar <patilchaitu22@gmail.com>
2022-02-22 23:35:31 +0530

    lazy loading basic example

---
+++ src/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+            <h4>About Component</h4>
+        </div>
+    )
+}
+
+export default About;
